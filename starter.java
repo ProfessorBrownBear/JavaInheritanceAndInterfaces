5345a166cdbdc54425548fe382cba17031e96867
Peter Sigurdson <139134226+ProfessorBrownBear@users.noreply.github.com>
2023-08-03 15:34:47 -0400

    Update starter.java

--- starter.java
+++ starter.java
@@ -2,7 +2,6 @@ interface PetStore {
     String makeNoise(String typeOfPet);
 }
 
-
 public class Pet implements PetStore 
 {
     @Override
@@ -10,4 +9,20 @@ public class Pet implements PetStore
         return "I am a " + typeOfPet + " and I go " + getNoise(typeOfPet);
     } 
 
+    public static void main(String[] args) {
+        System.out.println("Starting Program");
+    }
+
+    String getNoise(String typeOfPet) {
+        switch(typeOfPet) {
+            case "dog":
+                return "woof";
+            case "cat":
+                return "meow";
+            case "bird":
+                return "chirp";
+            default:
+                return "I don't know what I am";
+        }
+    }   
 }
